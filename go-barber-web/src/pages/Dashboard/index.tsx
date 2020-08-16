1c6d6c8891169b58153006e19afb3d0ed25965d1
sirmd <mate.deitos@hotmail.com>
2020-08-15 22:29:40 -0300

    #72 - Criação do calendário e estilização

--- go-barber-web/src/pages/Dashboard/index.tsx
+++ go-barber-web/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import { FiPower, FiClock } from 'react-icons/fi';
+import DayPicker, { DayModifiers } from 'react-day-picker';
 import { useAuth } from '../../hooks/AuthContext';
 import { useToast } from '../../hooks/ToastContext';
 import logoImg from '../../assets/logo.svg';
@@ -14,11 +15,16 @@ import {
   Appointment,
   Calendar,
 } from './styles';
+import 'react-day-picker/lib/style.css';
 
 const Dashboard: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const { signOut, user } = useAuth();
   const { addToast } = useToast();
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) setSelectedDate(day);
+  }, []);
 
   const handleSignOut = useCallback(async () => {
     try {
@@ -120,7 +126,32 @@ const Dashboard: React.FC = () => {
             </Appointment>
           </Section>
         </Schedule>
-        <Calendar />
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()} // Não irá permitir selecionar meses passados
+            disabledDays={[{ daysOfWeek: [0, 6] }]} // Dias da semana desabilitados, sábado e domingo
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
+            }}
+            selectedDays={selectedDate}
+            onDayClick={handleDateChange}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Setembro',
+              'Outubro',
+              'Novembro',
+              'Dezembro',
+            ]}
+          />
+        </Calendar>
       </Content>
     </>
   );
