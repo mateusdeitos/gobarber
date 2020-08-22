fc4ab8bce258407cf4d944d1a7a620fbebd3f5f1
sirmd <mate.deitos@hotmail.com>
2020-08-22 17:01:03 -0300

    Criando o Header no mobile

--- appgobarber/src/pages/Dashboard/index.tsx
+++ appgobarber/src/pages/Dashboard/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { View, Button } from 'react-native';
+import { Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar } from './styles';
 
 import { useAuth } from '../../hooks/auth';
+import { useNavigation } from '@react-navigation/native';
+
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const { navigate } = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
-    <View>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {"\n"}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
+    </Container>
   )
 }
 
